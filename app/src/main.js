9a75e8b44afcfbf8b8899d12950061d781aa2d41
Andy-Zhang7223 <andyz127@nycstudents.net>
2025-12-18 12:31:45 -0500

    12/18/2025

--- app/src/main.js
+++ app/src/main.js
@@ -3,14 +3,22 @@ import "./style.css";
 //Use a fetch call to get the api, not copy and paste the api into your code
 //btw this is the api, https://riskofrain2api.herokuapp.com/api/everyItem
 
-function inject(item) {
-  const test = document.querySelector(".test");
-  test.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="card">
-    <img src=${item.itemImage} alt="">
-    </div>`
-  );
-}
+const api = await fetch(
+  "https://riskofrain2api.herokuapp.com/api/everyItem"
+).then((response) => {
+  console.log(response);
+});
 
-api.forEach((item) => inject(item));
+// console.log(api);
+
+// function inject(item) {
+//   const test = document.querySelector(".test");
+//   test.insertAdjacentHTML(
+//     "afterbegin",
+//     `<div class="card">
+//     <img src=${item.itemImage} alt="">
+//     </div>`
+//   );
+// }
+
+// api.forEach((item) => inject(item));
