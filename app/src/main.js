466ea198f28dd3a6a5695f28e8997ca81f7235c2
Andy-Zhang7223 <andyz127@nycstudents.net>
2025-12-19 12:30:58 -0500

    12/19/2025

--- app/src/main.js
+++ app/src/main.js
@@ -3,9 +3,7 @@ import "./style.css";
 //Use a fetch call to get the api, not copy and paste the api into your code
 //btw this is the api, https://riskofrain2api.herokuapp.com/api/everyItem
 
-const api = await fetch(
-  "https://riskofrain2api.herokuapp.com/api/everyItem"
-)
+const api = await fetch("https://riskofrain2api.herokuapp.com/api/everyItem");
 //Below this line to the star is code that can console.log the api
 // .then((response) => {
 //   const apiurl = response.url;
@@ -13,24 +11,32 @@ const api = await fetch(
 //*
 
 //Turn api url into variable: (Priority Zero, Get this to work)
-const apiurl = api.url
+const apiurl = api.url;
 
 //test:
 console.log(apiurl);
 
 //Run try catch function here: (Priority One, Use this as a guard to yourself)
 
+//Attempting to print all the data:
+const apidata = await api.json();
+//Test:
+console.log(apidata);
+
 // console.log(api);
 
 //Inject function and other schtuff
-// function inject(item) {
-//   const test = document.querySelector(".test");
-//   test.insertAdjacentHTML(
-//     "afterbegin",
-//     `<div class="card">
-//     <img src=${item.itemImage} alt="">
-//     </div>`
-//   );
-// }
+function inject(item) {
+  const test = document.querySelector(".test");
+  test.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="card">
+    <img class="itemimg" src=${item.itemImage} alt="">
+    <h1 class="itemname">${item.itemName}</h1>
+    <h2 class="itemdescription">${item.description}</h2>
+    <h2 class="itemscaling">${item.stackType}</h2>
+    </div>`
+  );
+}
 
-// api.forEach((item) => inject(item));
+apidata.forEach((item) => inject(item));
